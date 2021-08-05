c6f951a49afe4f283783befcf08b342b1be1669b
Karan Dhillon <karanpal.dhillon@gmail.com>
2021-08-05 14:20:45 +0530

    initial commit

---
+++ js/charts-main.js
@@ -0,0 +1,434 @@
+window.addEventListener('load', (event) => {
+    barChartGrouped();
+    barChart();
+    horizontalChart();
+    horizontalGroupedChart();
+    pieChart();
+    lineChart();
+});
+
+function barChartGrouped() {
+    const ctx4 = document.getElementById('bar-chart-grouped').getContext('2d');
+    const myChart = new Chart(ctx4, {
+        type: 'bar',
+        plugins: [ChartDataLabels],
+        data: {
+            labels: ['1900', '1950', '1999', '2050'],
+            datasets: [
+                {
+                    label: 'Africa',
+                    backgroundColor: 'rgba(255,99,132,1)',
+                    data: [133, 221, 783, 2478],
+                },
+                {
+                    label: 'Europe',
+                    backgroundColor: 'rgba(54, 162, 235, 1)',
+                    data: [408, 547, 675, 1734],
+                },
+            ],
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                datalabels: {
+                    anchor: 'end',
+                    align: 'end',
+                    formatter: (value) => {
+                        return value + '%';
+                    },
+                    font: {
+                        size: 16,
+                        weight: 'bold',
+                    },
+                },
+            },
+            title: {
+                display: true,
+                text: 'Population growth (millions)',
+            },
+            legend: {
+                display: false,
+            },
+            scales: {
+                x: {
+                    grid: {
+                        display: false,
+                    },
+                },
+
+                y: {
+                    grid: {
+                        display: false,
+                    },
+                },
+            },
+        },
+    });
+}
+function barChart() {
+    const ctx = document.getElementById('barChart').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'bar',
+        plugins: [ChartDataLabels],
+        scaleLineColor: 'rgba(0,0,0,0)',
+        data: {
+            labels: [
+                '2015-01',
+                '2015-02',
+                '2015-03',
+                '2015-04',
+                '2015-05',
+                '2015-06',
+                '2015-07',
+                '2015-08',
+                '2015-09',
+                '2015-10',
+                '2015-11',
+                '2015-12',
+            ],
+            datasets: [
+                {
+                    label: '# of',
+                    data: [12, 19, 3, 5, 21, 3, 16, 3, 5, 6, 2, 11],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)',
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)',
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)',
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)',
+                    ],
+                    borderWidth: 1,
+                },
+            ],
+        },
+        options: {
+            plugins: {
+                datalabels: {
+                    anchor: 'end',
+                    align: 'end',
+                    formatter: (value) => {
+                        return value + '%';
+                    },
+                    font: {
+                        size: 16,
+                        weight: 'bold',
+                    },
+                },
+            },
+            showDatapoints: true,
+            tooltips: {
+                enabled: false,
+            },
+            title: {
+                display: false,
+                text: 'Population growth (millions)',
+            },
+            legend: {
+                display: false,
+            },
+            hover: {
+                animationDuration: 0,
+            },
+            scales: {
+                x: {
+                    grid: {
+                        display: false,
+                    },
+                },
+
+                y: {
+                    grid: {
+                        display: false,
+                    },
+                },
+            },
+        },
+    });
+}
+
+function horizontalChart() {
+    const ctx = document.getElementById('HorizontalBarchart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        plugins: [ChartDataLabels],
+        data: {
+            labels: [
+                '2015-01',
+                '2015-02',
+                '2015-03',
+                '2015-04',
+                '2015-05',
+                '2015-06',
+                '2015-07',
+                '2015-08',
+                '2015-09',
+                '2015-10',
+                '2015-11',
+                '2015-12',
+            ],
+            datasets: [
+                {
+                    label: '# of',
+                    data: [12, 19, 3, 5, 2, 3, 20, 3, 5, 6, 2, 1],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)',
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)',
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)',
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)',
+                    ],
+                    borderWidth: 1,
+                    barThickness: 22,
+                    barPercentage: 0.5,
+                },
+            ],
+        },
+        options: {
+            indexAxis: 'y',
+            scales: {
+                x: {
+                    grid: {
+                        display: false,
+                    },
+                },
+
+                y: {
+                    grid: {
+                        display: false,
+                    },
+                },
+            },
+            title: {
+                display: false,
+                text: 'Population growth (millions)',
+            },
+            legend: {
+                display: false,
+            },
+
+            hover: {
+                animationDuration: 0,
+            },
+            plugins: {
+                datalabels: {
+                    anchor: 'end',
+                    align: 'end',
+                    offset: '5',
+                    formatter: (value) => {
+                        return value + '%';
+                    },
+                    font: {
+                        size: 18,
+                        weight: 'bold',
+                    },
+                },
+            },
+        },
+    });
+}
+
+function horizontalGroupedChart() {
+    const ctx5 = document.getElementById('bar-chart-Horizontalgrouped').getContext('2d');
+    const myChart = new Chart(ctx5, {
+        type: 'bar',
+        plugins: [ChartDataLabels],
+        data: {
+            labels: ['1900', '1950', '1999', '2050'],
+            datasets: [
+                {
+                    label: 'Africa',
+                    backgroundColor: 'rgba(255,99,132,1)',
+                    data: [133, 221, 783, 2478],
+                },
+                {
+                    label: 'Europe',
+                    backgroundColor: 'rgba(54, 162, 235, 1)',
+                    data: [408, 547, 675, 734],
+                },
+            ],
+        },
+        options: {
+            indexAxis: 'y',
+            plugins: {
+                datalabels: {
+                    anchor: 'end',
+                    align: 'start',
+                    offset: '0',
+                    formatter: (value) => {
+                        return value + '%';
+                    },
+                    font: {
+                        size: 14,
+                        weight: 'bold',
+                    },
+                },
+            },
+            scales: {
+                x: {
+                    grid: {
+                        display: false,
+                    },
+                },
+
+                y: {
+                    grid: {
+                        display: false,
+                    },
+                },
+            },
+            title: {
+                display: false,
+                text: 'Population growth (millions)',
+            },
+            legend: {
+                display: false,
+            },
+        },
+    });
+}
+
+function pieChart() {
+    const ctx2 = document.getElementById('pieChart').getContext('2d');
+    const myChart2 = new Chart(ctx2, {
+        type: 'doughnut',
+        plugins: [ChartDataLabels],
+        data: {
+            labels: ['Africa', 'Asia', 'Europe', 'Latin America', 'North America'],
+            datasets: [
+                {
+                    label: 'Population (millions)',
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                    ],
+                    data: [2478, 5267, 734, 784, 433],
+                },
+            ],
+        },
+        options: {
+            plugins: {
+                datalabels: {
+                    anchor: 'end',
+                    align: 'end',
+                    formatter: (value) => {
+                        return value + '%';
+                    },
+                    font: {
+                        size: 18,
+                        weight: 'bold',
+                    },
+                },
+                legend: {
+                    position: 'right',
+                },
+            },
+            title: {
+                display: true,
+                text: 'Predicted world population (millions) in 2050',
+            },
+            hover: {
+                animationDuration: 0,
+            },
+        },
+    });
+}
+
+function lineChart() {
+    const ctx = document.getElementById('lineChart').getContext('2d');
+    const myChart1 = new Chart(ctx, {
+        type: 'line',
+        plugins: [ChartDataLabels],
+        data: {
+            labels: [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999, 2050],
+            datasets: [
+                {
+                    data: [12, 19, 3, 5, 2, 3, 20, 3, 5, 6, 2, 1],
+                    label: 'Africa',
+                    borderColor: '#3e95cd',
+                    fill: false,
+                },
+            ],
+        },
+        options: {
+            plugins: {
+                datalabels: {
+                    anchor: 'top',
+                    align: 'top',
+                    offset: '5',
+                    formatter: (value) => {
+                        return value + '%';
+                    },
+                    font: {
+                        size: 16,
+                        weight: 'bold',
+                    },
+                },
+            },
+            title: {
+                display: false,
+                text: 'Population growth (millions)',
+            },
+            legend: {
+                display: false,
+            },
+            scales: {
+                x: {
+                    grid: {
+                        display: false,
+                    },
+                },
+
+                y: {
+                    grid: {
+                        display: false,
+                    },
+                },
+            },
+        },
+    });
+}
